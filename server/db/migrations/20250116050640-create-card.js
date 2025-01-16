ec31d91fad1827c77ae7bb49f387ea933da65e95
unknown <bestpsyholog@gmail.com>
2025-01-16 08:16:27 +0300

    db_finish

---
+++ server/db/migrations/20250116050640-create-card.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Cards', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      english: {
+        type: Sequelize.STRING,
+      },
+      russian: {
+        type: Sequelize.STRING,
+      },
+      learned: {
+        type: Sequelize.BOOLEAN,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      imagePath: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Cards');
+  },
+};
